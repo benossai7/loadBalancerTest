ecd116ec884df5c246ce592fc67c77550a411e84
Ossai Benjamin <benossai7@gmail.com>
2023-09-02 10:46:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./app");
 require("dotenv").config();
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 8080;
 // const port = 3000;
 const server = http.createServer(app);
 
